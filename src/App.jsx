dae49b0f87a2d278e94cf78fd4cda2b883b095b0
thelazylazyigor72 <heartunderblade1337@ya.ru>
2023-02-25 20:04:27 +0300

    testing new setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Modal from './Modal'
-import Sidebar from './Sidebar'
-import Home from './Home'
+import React from "react";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 //помним что в индексе наш апп обернут в провайдер, соответственно все дети провайдера смогут получать контекст
 function App() {
-  return (
-    <>
-      <Home />
-      <Modal />
-      <Sidebar />
-    </>
-  )
+	return (
+		<>
+			<Home />
+			<Modal />
+			<Sidebar />
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/context.jsx
+++ src/context.jsx
@@ -1,43 +1,46 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
 
 //*Это вот как и говорилось в про-типсах = выносить определение контекста и связанной с ним хуеты в отдельный файл
 //определяем объект контекста
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 //*внимательно! мы создаем компонент провайдер-обертку и экспортим его, чтобы не в основном коде каждый раз чет описывать
 //для этого же мы передаем проп чилдрен чтобы как мы видим пихать в провайдер что угодно
-const AppProvider = ({children}) => {
-    //определяем стейты и закрыть/открыть функции для модальника и сайдбара и передаем их в value 
-    //чтобы потом использовать их в любой точке приложения
-    const [isSidebarOpen,setIsSidebarOpen] = useState(false)
-    const [isModalOpen,setIsModalOpen] = useState(false)
+const AppProvider = ({ children }) => {
+	//определяем стейты и закрыть/открыть функции для модальника и сайдбара и передаем их в value
+	//чтобы потом использовать их в любой точке приложения
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openSidebar = () => {
-        setIsSidebarOpen(true)
-    }
-    const closeSidebar = () => {
-        setIsSidebarOpen(false)
-    }
+	const openSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+	const closeSidebar = () => {
+		setIsSidebarOpen(false);
+	};
 
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
-    const closeModal = () => {
-        setIsModalOpen(false)
-    }
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
-
-    return <AppContext.Provider value={{
-        isModalOpen,
-        isSidebarOpen,
-        openModal,
-        openSidebar,
-        closeModal,
-        closeSidebar
-    }}>
-        {children}
-    </AppContext.Provider>
-}
+	return (
+		<AppContext.Provider
+			value={{
+				isModalOpen,
+				isSidebarOpen,
+				openModal,
+				openSidebar,
+				closeModal,
+				closeSidebar,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+};
 
 //custom hook
 //этот хук - это просто пример имплементации от Джона, чтобы каждый раз в нужных файлах не импортить и объект-контекста
@@ -46,7 +49,7 @@ const AppProvider = ({children}) => {
 //но в целом можно было обойтись и без этого, это так, хенди типсы.
 //важно что мы пишем use - иначе это не считается кастомным хуком
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+	return useContext(AppContext);
+};
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
